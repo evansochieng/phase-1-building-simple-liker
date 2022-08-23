fcce36ed93a92442feab8686b2dbd2df3aee3b37
Evans Ochieng <evansochieng121@gmail.com>
2022-08-23 17:55:37 +0300

    Completed simple liker

--- main.js
+++ main.js
@@ -4,6 +4,51 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Define like button states
+// The value of empty heart is full heart and vice versa
+const likeStates = {
+  [EMPTY_HEART]: FULL_HEART,
+  [FULL_HEART]: EMPTY_HEART
+};
+
+// Define the color states of the like button
+// empty heart becomes red if clicked clicked and vice versa
+const heartColors = {
+  '': 'red',
+  'red': ''
+};
+
+// Add an event listener to the like buttons
+// Change the  colors of the hearts 
+document.addEventListener('DOMContentLoaded', () => {
+  const likeBtn = document.querySelectorAll('.like-glyph');
+  for (const btn of likeBtn){
+    btn.addEventListener('click', likePost);
+  }
+})
+
+// Create a function that change the color
+function likePost(event){
+  const heart = event.target; //get the tag (element) with the like hearts
+
+  // Invoke mimicServerCall() when empty heart is clicked
+  mimicServerCall()
+  .then( (serverMessage) => {
+    alert(serverMessage); //alert the message from the server
+
+    // Change the color of the hearts
+    heart.innerText = likeStates[heart.innerText]; //change the heart displayed
+    heart.style.color = heartColors[heart.style.color]; //change background color of liked heart and vice versa
+  })
+  .catch( (error) => {
+    document.getElementById('modal').removeAttribute('hidden'); //grab and display error div
+    const errorPage = document.getElementById('modal-message'); //grab error paragraph
+    errorPage.textContent = error.message; //show error message from the server
+    setTimeout( () => { //set timeout for the error being displayed. The error to disappear after 3 seconds
+      document.getElementById('modal').hidden = true
+    }, 3000);
+  })
+}
 
 
 
