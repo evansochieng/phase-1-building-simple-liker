eb979bc19cfec7d98dca490faac9b9259926a6ce
Evans Ochieng <evansochieng121@gmail.com>
2022-08-29 10:24:45 +0300

    Sorted hidden class error

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function likePost(event){
     heart.style.color = heartColors[heart.style.color]; //change background color of liked heart and vice versa
   })
   .catch( (error) => {
-    document.getElementById('modal').removeAttribute('hidden'); //grab and display error div
+    document.getElementById('modal').removeAttribute('.hidden'); //grab and display error div
     const errorPage = document.getElementById('modal-message'); //grab error paragraph
     errorPage.textContent = error.message; //show error message from the server
     setTimeout( () => { //set timeout for the error being displayed. The error to disappear after 3 seconds
